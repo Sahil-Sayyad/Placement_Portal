8e6fef1df9bd257ce2e4d72acd9700f453497465
Software Engineer <96423459+Sahil-Sayyad@users.noreply.github.com>
2023-07-28 11:30:51 +0530

    Update job_Controller.js

--- controllers/job_Controller.js
+++ controllers/job_Controller.js
@@ -1,10 +1,11 @@
 
-const url = 'https://www.themuse.com/api/public/jobs?category=Software%20Engineer&category=Software%20Engineering&page=2';
+// const url = 'https://www.themuse.com/api/public/jobs?category=Software%20Engineer&category=Software%20Engineering&page=2';
 
 // Route to fetch jobs 
 // fetches real available jobs
 module.exports.Fetch = async (req, res) => {
   try {
+    const url = 'https://www.themuse.com/api/public/jobs?category=Software%20Engineer&category=Software%20Engineering&page=2';
     const response = await fetch(url);
     const data = await response.json();
     return res.render('job_list', {
